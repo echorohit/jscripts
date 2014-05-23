860830363fcd9718aba4055830c6c443f5a7d2db
rohit <rohit@rohit.(none)>
2014-05-23 13:54:52 +0530

    File for objects in Javascript

---
+++ object.js
@@ -0,0 +1,10 @@
+/*
+An object is very similar to an array but with the difference that you define the keys
+yourself. You're not limited to using only numeric indexes but can use friendlier
+keys, such as first_name, age, and so on.
+Let's take a look at a simple object and examine its parts:
+*/
+var hero = {
+  breed: 'Turtle',
+  occupation: 'Ninja'
+};
